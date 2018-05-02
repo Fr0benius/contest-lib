512151aaa28afe53691f8b105e7dc2b71cddf234
Oleg Rudenko <oxr907@psu.edu>
2018-05-01 21:40:34 -0400

    Added contest template

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstring>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+#define pb push_back
+#define fst first
+#define snd second
+
+typedef long long ll;
+typedef pair<int,int> pii;
+template<typename T> using min_queue=priority_queue<T,vector<T>,greater<T>>;
+
+const ll MOD=1e9+7;
+
+int main(){
+  ios::sync_with_stdio(0);cin.tie(0);
+
+  return 0;
+}

08efe330b9ee5ee0c795414b206ad9c98adfb5f0
Oleg Rudenko <zhulik@Olegs-MacBook-Pro.local>
2018-05-12 10:32:40 -0400

    Reformatted template

--- template.cpp
+++ template.cpp
@@ -27,7 +27,7 @@ template<typename T> using min_queue=priority_queue<T,vector<T>,greater<T>>;
 const ll MOD=1e9+7;
 
 int main(){
-  ios::sync_with_stdio(0);cin.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);
 
-  return 0;
+    return 0;
 }

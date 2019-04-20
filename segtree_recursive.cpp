5260ee3f49dd510c854fd144f5a6d3cd966a1505
Oleg Rudenko <orudenko@orudenko-macbookpro.roam.corp.google.com>
2019-04-20 12:50:25 -0700

    Lazy segtree fixes

--- segtree_recursive.cpp
+++ segtree_recursive.cpp
@@ -9,6 +9,11 @@ const int MAXN=100100;
 int N;
 node seg[4*MAXN];
 
+inline void combine(int ix){
+  
+}
+
+//Array indices from 0 to N-1
 void build(int ix=1,int l=0,int r=N){
   if(r-l<=1){
     //initiate singletons here
@@ -17,32 +22,42 @@ void build(int ix=1,int l=0,int r=N){
   build(ix*2,l,(l+r)/2);
   build(ix*2+1,(l+r)/2,r);
 
-  //Combine ix*2 and ix*2+1 into ix here
+  combine(ix);
 }
 
-inline void push(int ix){//push lazy computation to children
+inline void push(int ix,bool nonleaf){//apply lazy computation and push to children
   if(seg[ix].lazy==0) return;
 }
 
-void update(int a,int b,int v,int ix=1,int l=0,int r=N){
-  if(r-l>1) push(ix);
+void single_update(int k,int v,int ix=1,int l=0,int r=N){
+  if(k<l || k>=r) return;
+  if(r-l==1){
+    //Update here
+    return;
+  }
 
+  single_update(k,v,2*ix,l,(l+r)/2);
+  single_update(k,v,2*ix+1,(l+r)/2,r);
+  combine(ix);
+}
+void range_update(int a,int b,int v,int ix=1,int l=0,int r=N){
   if(b<=l || a>=r) return;
 
   if(a<=l && r<=b){
-    //lazy update here
+    //Update here
+    push(ix,r-l>1);
 
     return;
   }
 
-  update(a,b,v,2*ix,l,(l+r)/2);
-  update(a,b,v,2*ix+1,(l+r)/2,r);
-
-  //Combine ix*2 and ix*2+1 into ix here
+  push(ix,r-l>1);
+  range_update(a,b,v,2*ix,l,(l+r)/2);
+  range_update(a,b,v,2*ix+1,(l+r)/2,r);
+  combine(ix);
 }
 
 int query(int a,int b,int ix=1,int l=0,int r=N){
-  if(r-l>1) push(ix);
+  push(ix,r-l>1);
 
   if(b<=l || a>=r) return 0;//return identity here
   if(a<=l && r<=b){

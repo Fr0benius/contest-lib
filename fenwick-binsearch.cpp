d44d55fd86454e92ac25b824558c9b07cc424ae9
Oleg Rudenko <shturzys@gmail.com>
2020-07-28 22:41:37 -0700

    Put Fenwick tree in a struct

--- fenwick-binsearch.cpp
+++ fenwick-binsearch.cpp
@@ -1,29 +1,32 @@
-const int MAXN=100100;
+template<typename T>
+struct BIT {
+  int N;
+  vector<T> bit;
 
-int N;
-int bit[MAXN];
+  BIT(int N_) : N(N_), bit(N_) { }
 
-inline int peek(int ix){
+  inline T peek(int ix){
     int res=0;
     for(;ix;ix-=ix&-ix) res+=bit[ix];
     return res;
-}
+  }
 
-inline void update(int ix,int v){
+  inline void update(int ix,T v){
     for(;ix<=N;ix+=ix&-ix) bit[ix]+=v;
-}
+  }
 
-// Find least k such that peek(k)>=v
-inline int lbound(int v){
+  // Find least k such that peek(k)>=v
+  inline int lbound(T v){
     if(v<=0) return 0;
 
     int cur=0;
     int p=1<<30;
 
     while(1){
-        while(p>0 && (cur+p>N || bit[cur+p]>=v)) p>>=1;
-        if(p==0) return cur+1;
-        cur+=p,v-=bit[cur],p>>=1;
+      while(p>0 && (cur+p>N || bit[cur+p]>=v)) p>>=1;
+      if(p==0) return cur+1;
+      cur+=p,v-=bit[cur],p>>=1;
     }
     return cur;
-}
+  }
+};

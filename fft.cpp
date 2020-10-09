881894948bb802952f9caa53fe531836a2c99b31
Oleg Rudenko <shturzys@gmail.com>
2020-10-09 14:20:17 -0700

    Add iterative fft

--- fft.cpp
+++ fft.cpp
@@ -1,9 +1,45 @@
 // Dir is 1 or -1 depending on direction.
 // To complete the inverse fft, divide all coefficients by N.
-// TODO: make iterative, figure out precision
+// TODO: figure out precision
 
-using cd = complex<double>;
 void fft(vector<cd>& a, const int N, const int dir = 1) {
+  for (int i = N; i < a.size(); i++) {
+    a[i%N] += a[i];
+  }
+  a.resize(N);
+
+  vector<cd> roots(N/2);
+  static const double PI = acos(-1.0);
+  roots[0] = 1.0;
+  for (int i=1; i<N/2; i++) {
+    roots[i] = polar(1.0, PI * 2 * i * dir / N);
+  }
+
+  auto rev = [N] (int n) {
+    int res = 0;
+    for (n += N;n > 1; n >>= 1) {
+      res = res * 2 + (n&1); 
+    }
+    return res;
+  };
+   
+  for (int i=0; i<N; i++) {
+    int j = rev(i);
+    if (i < j) swap(a[i], a[j]);
+  }
+  for (int gap = 1; gap < N; gap <<= 1) {
+    for (int i = 0; i < N; i += gap*2) {
+      for (int j = 0; j < gap; j++) {
+        cd root = roots[N/(gap*2)*j];
+        tie(a[i+j], a[i+j+gap]) = 
+          pair{a[i+j] + a[i+j+gap]*root, a[i+j] - a[i+j+gap]*root};
+      }
+    }
+  }
+}
+
+// Recursive version. Much slower so probably best to just use the other one.
+void fft_rec(vector<cd>& a, const int N, const int dir = 1) {
   static const double PI = acos(-1.0);
   for (int i = N; i < a.size(); i++) {
     a[i%N] += a[i];
@@ -15,8 +51,8 @@ void fft(vector<cd>& a, const int N, const int dir = 1) {
     l[i] = a[2*i];
     r[i] = a[2*i+1];
   }
-  fft(l, N/2, dir);
-  fft(r, N/2, dir);
+  fft_rec(l, N/2, dir);
+  fft_rec(r, N/2, dir);
   for (int i = 0; i < N/2; i++) {
     cd root = {cos(PI * 2 * i * dir / N), sin(PI * 2 * i * dir / N)};
     a[i] = l[i] + root * r[i];

c5b05d46aa5dc979b9a8ce7e8a4d39d74f40f117
Oleg Rudenko <shturzys@gmail.com>
2022-05-31 17:05:32 -0700

    reformat, pass reference

--- src/sparse-table.cpp
+++ src/sparse-table.cpp
@@ -1,22 +1,22 @@
-template<typename T>
+template <typename T>
 struct sparse {
   vector<T> table[32];
 
-  sparse() { }
-  sparse(vector<T> v) { init(v); }
-  void init(vector<T> v) {
+  sparse() {}
+  sparse(const vector<T>& v) { init(v); }
+  void init(const vector<T>& v) {
     table[0] = v;
-    for(size_t i=1; (1<<i) <= v.size(); i++) {
-      table[i].resize(v.size()-(1<<i)+1);
-      for(int j=0;j<table[i].size();j++) {
-        table[i][j] = min(table[i-1][j], table[i-1][j + (1<<(i-1))]);
+    for (size_t i = 1; (1 << i) <= v.size(); i++) {
+      table[i].resize(v.size() - (1 << i) + 1);
+      for (int j = 0; j < table[i].size(); j++) {
+        table[i][j] = min(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
       }
     }
   }
 
   // Query the range [l,r).
   T query(int l, int r) {
-    int i = 31-__builtin_clz(r-l); // This is the floor of log_2(r-l+1)
-    return min(table[i][l], table[i][r-(1<<i)]);
+    int i = 31 - __builtin_clz(r - l);  // This is the floor of log_2(r-l+1)
+    return min(table[i][l], table[i][r - (1 << i)]);
   }
 };

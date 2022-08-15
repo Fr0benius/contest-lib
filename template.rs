03159af7a1d6e934282cbcfe02eb58be0e9b998b
Oleg Rudenko <shturzys@gmail.com>
2022-08-14 21:00:21 -0700

    debug in rust template

--- template.rs
+++ template.rs
@@ -1,22 +1,27 @@
 #![allow(unused_imports)]
 #![allow(unused_must_use)]
 #![allow(non_snake_case)]
+#![allow(unused_macros)]
+#![allow(clippy::too_many_arguments)]
+#![allow(clippy::many_single_char_names)]
+#![allow(clippy::needless_range_loop)]
 
-use std::collections::{VecDeque, HashSet, HashMap};
-use std::cmp::{min, max, Reverse};
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::io::{self, prelude::*};
 use std::str;
-use std::marker::PhantomData;
- 
+
 fn solve<R: BufRead, W: Write>(sc: &mut Scanner<R>, wr: &mut W) {
+    
 }
 
+use std::marker::PhantomData;
 macro_rules! recursive_function {
     ($name: ident, $trait: ident, ($($type: ident $arg: ident,)*)) => {
         pub trait $trait<$($type, )*Output> {
             fn call(&mut self, $($arg: $type,)*) -> Output;
         }
- 
+
         pub struct $name<F, $($type, )*Output>
         where
             F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
@@ -26,7 +31,7 @@ macro_rules! recursive_function {
             )*
             phantom_output: PhantomData<Output>,
         }
- 
+
         impl<F, $($type, )*Output> $name<F, $($type, )*Output>
         where
             F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
@@ -40,7 +45,7 @@ macro_rules! recursive_function {
                 }
             }
         }
- 
+
         impl<F, $($type, )*Output> $trait<$($type, )*Output> for $name<F, $($type, )*Output>
         where
             F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
@@ -53,7 +58,7 @@ macro_rules! recursive_function {
         }
     }
 }
- 
+
 recursive_function!(RecursiveFunction0, Callable0, ());
 recursive_function!(RecursiveFunction, Callable, (Arg arg,));
 recursive_function!(RecursiveFunction2, Callable2, (Arg1 arg1, Arg2 arg2,));
@@ -65,6 +70,18 @@ recursive_function!(RecursiveFunction7, Callable7, (Arg1 arg1, Arg2 arg2, Arg3 a
 recursive_function!(RecursiveFunction8, Callable8, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8,));
 recursive_function!(RecursiveFunction9, Callable9, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9,));
 
+#[macro_export]
+macro_rules! dbg{
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        eprintln!(
+            concat!("{}:{}:{}: ",$(stringify!($a), " = {:?}, "),*),
+            file!(), line!(), column!(), $($a),*
+        );
+        #[cfg(not(debug_assertions))]
+        {};
+    }
+}
 struct Scanner<R> {
     reader: R,
     buf_str: Vec<u8>,
